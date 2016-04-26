0d4118a453e8b87c6b95b58ad686b115a500494c
zhaoyc <zhzyc142@163.com>
2016-04-26 16:35:09 +0800

    debug

--- chat.js
+++ chat.js
@@ -74,7 +74,8 @@ chat.on('connection' ,function(socket){
           {$sort:  {creationDate: -1}}
         ], function(err, unread_messages){
           Room.aggregate([
-            {$match: {$and : [{owner: (socket.userid).toString()},  {isBase: true}]}}
+            {$match: {$and : [{owner: (socket.userid).toString()},  {isBase: true}]}},
+            {$project: {Logo: 1,  lastMessage: 1, fromUserId: 1, toUserId: 1, body:1}}
             ], function(err, baseroom){
               socket.emit("server_notice", {action:"login", type: "success", errcode: 200, data: {unread_messages: unread_messages, baseroom: baseroom} })
             })
